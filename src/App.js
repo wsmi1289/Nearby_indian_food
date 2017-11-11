9167cf68e6af0e76f4dec0a16963a6e0f32a0936
will <wsmi1289@gmail.com>
2017-11-11 11:15:03 -0500

    indian food finder
    Google maps with react Nearby indian food

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/*global google*/
+import React, { Component } from 'react';
+import Map from './Map';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
